8a105ba563226f98cc746b8a04b11dff9ec06475
Janet Spellman <janetspellman13@gmail.com>
2025-12-14 20:45:28 -0800

    refactoring: switching to next/font/google to get lighter woff font files

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,24 +1,15 @@
-import localFont from "next/font/local";
+import { Open_Sans, Dancing_Script } from "next/font/google";
 
-export const openSans = localFont({
-  src: [
-    {
-      path: "./fonts/OpenSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/OpenSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  display: "swap", // prevents invisible text
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
   variable: "--font-open-sans",
 });
 
-export const dancingScript = localFont({
-  src: "./fonts/DancingScript-Regular.ttf",
+export const dancingScript = Dancing_Script({
+  subsets: ["latin"],
+  weight: ["400"],
   display: "swap",
   variable: "--font-dancing-script",
 });
